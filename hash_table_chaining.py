c44832abaa2efaafe4ceb9073897047f0f119c71
grat2 <gf310031414@gmail.com>
2017-06-04 15:17:25 -0700

    did some stuff

--- hash_table_chaining.py
+++ hash_table_chaining.py
@@ -1,20 +1,23 @@
 # a HashTable is a HashTable(positions)
 class HashTable:
-    def __init__(self, size = 8, slots = [None] * 8, data = [None] * 8, hashFunc = None):
+    def __init__(self, size = 8, keys = [None] * 8, data = [None] * 8):
         self.size = size # an integer
-        self.slots = slots # 
-        self.data = data
+        self.keys = keys # list that contains keys of actual data
+        self.data = data # list that contains actual data
     def __eq__(self, other):
         return (type(other) == HashTable
                 and self.size == other.size
-                and self.slots == other.slots
+                and self.keys == other.keys
                 and self.data == other.data)
     def __repr__(self):
-        return "HashTable({!r}, {!r}, {!r})".format(self.size, self.slots, self.data)
+        return "HashTable({!r}, {!r}, {!r})".format(self.size, self.keys, self.data)
+    # key length ->
 
 # None -> HashTable
 # returns an empty HashTable
 def empty_hash_table():
     return HashTable()
 
-# HashTable key item
+# HashTable key item -> HashTable
+# inserts a value based on its hashed key value
+def insert(h_t, key, item):
